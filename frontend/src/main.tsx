4690a33de78c6b37477e721aa0e2e7760ccd69c3
Vincent Adrian <vincentquitoriano38@gmail.com>
2026-06-30 21:53:20 +0800

    feat: add Lenis smooth scrolling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ReactLenis } from 'lenis/react'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <ReactLenis root options={{ lerp: 0.1, duration: 1.2, smoothWheel: true }}>
+    <App />
+  </ReactLenis>,
+);
